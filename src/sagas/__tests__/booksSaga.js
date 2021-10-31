1b975aa369c3ad2fe3f3c75703a57ca142dddeac
salmaan-gurmani <salmaan.gurmani@gmail.com>
2021-10-31 23:53:53 +0100

    refactor(web_app):api test cases

--- src/sagas/__tests__/booksSaga.js
+++ src/sagas/__tests__/booksSaga.js
@@ -1,14 +1,20 @@
 import { runSaga } from 'redux-saga';
 
-import { getPage, handleBooksLoad } from '../booksSaga';
+import { getPage, handleBooksLoad, getItemsPerPage } from '../booksSaga';
 import * as api from '../../api'; // we'll mock the fetchBooks api
 import { setBooks, setError } from '../../actions';
 
 test('selector should return the desired page', () => {
-    const nextPage = 1;
-    const state = { nextPage };
+    const options = { page: 1 };
+    const state = { options };
     const res = getPage(state);
-    expect(res).toBe(nextPage);
+    expect(res).toBe(1);
+});
+test('selector should return the desired itemsPerPage', () => {
+    const options = { itemsPerPage: 30 };
+    const state = { options };
+    const res = getItemsPerPage(state);
+    expect(res).toBe(30);
 });
 
 test('should load and handle books in case of success', async () => {
@@ -20,17 +26,39 @@ test('should load and handle books in case of success', async () => {
     // so we will mock the fetchBooks api with jest
     // this will mutate the dependency which we may reset if other tests
     // are dependent on it
-    const mockedBooks = ['img1', 'img2'];
+    const mockedBooks = {
+        books: [
+            {
+                book_author: ['Ανώνυμος'],
+                book_pages: 104,
+                book_publication_city: 'Βενετία',
+                book_publication_country: 'Ιταλία',
+                book_publication_year: 1529,
+                book_title: 'Ο Αλέξανδρος ο Μακεδών',
+                id: 2086,
+            },
+            {
+                book_author: ['Ανώνυμος'],
+                book_pages: 32,
+                book_publication_city: 'Βενετία',
+                book_publication_country: 'Ιταλία',
+                book_publication_year: 1548,
+                book_title:
+                    'Διήγησις εις τας πράξεις του περιβοήτου στρατηγού των ρωμαίων μεγάλου Βελισαρίου',
+                id: 2060,
+            },
+        ],
+        type: 'BOOKS_LOAD_SUCCESS',
+    };
     api.fetchBooks = jest.fn(() => Promise.resolve(mockedBooks));
 
     const fakeStore = {
-        getState: () => ({ nextPage: 1 }),
+        getState: () => ({ options: { page: 1, itemsPerPage: 2 } }),
         dispatch: action => dispatchedActions.push(action),
     };
 
     // wait for saga to complete
-    await runSaga(fakeStore, handleBooksLoad).done;
-
+    await runSaga(fakeStore, handleBooksLoad).result;
     expect(api.fetchBooks.mock.calls.length).toBe(1);
     expect(dispatchedActions).toContainEqual(setBooks(mockedBooks));
 });
@@ -44,7 +72,7 @@ test('should handle image load errors in case of failure', async () => {
     api.fetchBooks = jest.fn(() => Promise.reject(error));
 
     const fakeStore = {
-        getState: () => ({ nextPage: 1 }),
+        getState: () => ({ options: { page: 1, itemsPerPage: 2 } }),
         dispatch: action => dispatchedActions.push(action),
     };
 
